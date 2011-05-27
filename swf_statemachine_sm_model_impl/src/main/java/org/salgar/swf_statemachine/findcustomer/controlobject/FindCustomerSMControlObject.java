2f83fc9353f97f0ec48ce856bf56a14564fe8164
Mehmet Salgar <salgarm@gmx.de>
2011-05-27 15:20:50 +0200

    slave machine changes

---
+++ swf_statemachine_sm_model_impl/src/main/java/org/salgar/swf_statemachine/findcustomer/controlobject/FindCustomerSMControlObject.java
@@ -0,0 +1,15 @@
+package org.salgar.swf_statemachine.findcustomer.controlobject;
+
+import java.io.Serializable;
+
+import org.salgar.statemachine.domain.ControlObject;
+
+public class FindCustomerSMControlObject implements ControlObject, Serializable {
+	private static final long serialVersionUID = -2200204569913643581L;
+
+	public void resetStateMachine() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
