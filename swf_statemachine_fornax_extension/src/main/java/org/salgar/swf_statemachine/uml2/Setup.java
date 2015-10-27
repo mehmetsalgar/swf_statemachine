149c3ee54918991ed7d1b8a9d1b04a99adac1a5a
Mehmet Salgar <mehmet.salgar@cybercon.de>
2015-10-27 15:00:08 +0100

    MWE2 Eclipse dependencies optimisation

--- swf_statemachine_fornax_extension/src/main/java/org/salgar/swf_statemachine/uml2/Setup.java
+++ swf_statemachine_fornax_extension/src/main/java/org/salgar/swf_statemachine/uml2/Setup.java
@@ -1,14 +1,12 @@
 package org.salgar.swf_statemachine.uml2;
 
-import org.eclipse.emf.ecore.EPackage;
-import org.eclipse.uml2.uml.UMLPackage;
-
-public class Setup extends org.eclipse.xtend.typesystem.uml2.Setup {
+public class Setup {
+	/**extends org.eclipse.xtend.typesystem.uml2.Setup {
 	private static final String UML2_300_NS_URI = "http://www.eclipse.org/uml2/3.0.0/UML";
 
 	@Override
 	public void setStandardUML2Setup(boolean b) {
 		super.setStandardUML2Setup(b);
 		EPackage.Registry.INSTANCE.put(UML2_300_NS_URI, EPackage.Registry.INSTANCE.get(UMLPackage.eINSTANCE.getNsURI()));
-	}
+	}*/
 }
